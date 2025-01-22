0956433c2ba7818ba696fe84073b7b72309821cf
fixads <admin@fixads.xyz>
2025-01-22 12:29:15 +0100

    Initial commit

---
+++ src/config/constants.ts
@@ -0,0 +1,21 @@
+export const SITE_CONFIG = {
+  name: 'FixAds',
+  domain: 'fixads.xyz',
+  description: 'Digital Marketing Agency for Performance Marketing',
+  email: 'admin@fixads.xyz',
+  social: {
+    twitter: 'https://twitter.com/fixads',
+    linkedin: 'https://linkedin.com/company/fixads',
+    instagram: 'https://instagram.com/fixads',
+  },
+  googleAnalyticsId: 'G-0XV5T4D4C1',
+  googleAdsId: 'AW-11453654058',
+  metaPixelId: '599339643038424',
+};
+
+export const EMAIL_CONFIG = {
+  serviceId: 'service_a4afkuq',
+  templateId: 'template_7rrl7y6', // Updated to correct template ID
+  publicKey: '715HPvd6oLEO5grl1',
+  toEmail: 'admin@fixads.xyz',
+};
